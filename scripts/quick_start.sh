289677c65969a939d1da9dba3ed202b9f40fad2c
xxuan01 <fengruoxuan@ruc.edu.cn>
2026-02-09 21:54:50 +0800

    update

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python -u scripts/quick_start.py \
     --output_dir log/quick_start \
     --model_size base \
-    --load_path /home/yaoguocai/code-frx/tactile/output_videomae_9.6_align_match_diff/checkpoint-4frames.pth \
+    --load_path checkpoints/checkpoint-4frames.pth \
     --num_frames 4 \
     --stride 2 \
     --model anytouch \
