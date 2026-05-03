f3b0e059f83597df7807bfb8e7fa11577647e90c
jingchao.hou@iit.it <jhou@fe01.priv.franklin.lan>
2026-05-03 18:30:00 +0200

    fix: remove cache files

--- scripts/quick_start.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python -u scripts/quick_start.py \
-    --output_dir log/quick_start \
-    --model_size base \
-    --load_path checkpoints/checkpoint-4frames.pth \
-    --num_frames 4 \
-    --stride 2 \
-    --model anytouch \
-    --data_sensor gelsight_mini
