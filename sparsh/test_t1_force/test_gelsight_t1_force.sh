3ab125eeca10e0bf254f1be02cf9443728bf1dfe
Jingchao Hou <hjc128@outlook.com>
2026-05-06 18:14:32 +0200

    a

--- sparsh/test_t1_force/test_gelsight_t1_force.sh
+++ sparsh/test_t1_force/test_gelsight_t1_force.sh
@@ -16,7 +16,7 @@ conda activate anytouch2
 python sparsh/test_t1_force/eval_t1_force.py \
   --run-dir sparsh/log/2026.04.30_13-56_gelsight_t1_force_anytouch_vitbase_1.0 \
   --checkpoint sparsh/log/2026.04.30_13-56_gelsight_t1_force_anytouch_vitbase_1.0/checkpoints/epoch-0051.pth \
-  --data-root set -euo pipefail \
+  --data-root /fastwork/jhou/AnyTouch2/datasets \
   --dataset-name flat/batch_1 \
   --dataset-name flat/batch_2 \
   --device cuda:0
