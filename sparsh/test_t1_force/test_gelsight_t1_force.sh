7d00f8118a0dca1968a534fe7583b85516410590
jingchao.hou@iit.it <jhou@fe01.priv.franklin.lan>
2026-05-06 17:39:38 +0200

    a

--- sparsh/test_t1_force/test_gelsight_t1_force.sh
+++ sparsh/test_t1_force/test_gelsight_t1_force.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-set -euo pipefail
+
+echo "Running on:"
+hostname
+
+echo "GPU status:"
+nvidia-smi
+
+
+source ~/.bashrc
+conda activate anytouch2
+
+
 
 python sparsh/test_t1_force/eval_t1_force.py \
   --run-dir sparsh/log/2026.04.30_13-56_gelsight_t1_force_anytouch_vitbase_1.0 \
   --checkpoint sparsh/log/2026.04.30_13-56_gelsight_t1_force_anytouch_vitbase_1.0/checkpoints/epoch-0051.pth \
-  --data-root /home/jhou-iit.local/AnyTouch2/datasets \
+  --data-root set -euo pipefail \
   --dataset-name flat/batch_1 \
   --dataset-name flat/batch_2 \
   --device cuda:0
