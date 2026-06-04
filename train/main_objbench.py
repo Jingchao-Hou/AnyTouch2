3f343dfd822772affb775098902bf70b66fec1f1
Jingchao Hou <hjc128@outlook.com>
2026-06-04 16:33:07 +0200

    a

--- train/main_objbench.py
+++ train/main_objbench.py
@@ -193,15 +193,23 @@ def main(args):
         cloth_class_metrics = test_stats.pop("cloth_class_metrics", None)
 
         if cloth_class_metrics is not None and args.output_dir and misc.is_main_process():
+            save_data = {
+                "acc1": test_stats["acc1"],
+                "cloth_class_metrics": cloth_class_metrics,
+            }
             with open(os.path.join(args.output_dir, f"cloth_class_metrics_epoch_{epoch}.json"), "w", encoding="utf-8") as f:
-                json.dump(cloth_class_metrics, f, indent=2)
+                json.dump(save_data, f, indent=2)
 
         
         tag_class_metrics = test_stats.pop("tag_class_metrics", None)
 
         if tag_class_metrics is not None and args.output_dir and misc.is_main_process():
+            save_data = {
+                "acc1": test_stats["acc1"],
+                "tag_class_metrics": tag_class_metrics,
+            }
             with open(os.path.join(args.output_dir, f"tag_class_metrics{epoch}.json"), "w", encoding="utf-8") as f:
-                json.dump(tag_class_metrics, f, indent=2)
+                json.dump(save_data, f, indent=2)
 
 
         print(f"Accuracy of the network on the {len(dataset_val)} test images: {test_stats['acc1']:.1f}%")
